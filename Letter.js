a16bb5703f9639e0632f28b2c7934ded6242694e
Edyta <dudek002@hotmail.com>
2018-02-13 17:56:07 -0600

    fixed Letter.js

--- Letter.js
+++ Letter.js
@@ -1,140 +1,68 @@
 var inquirer = require("inquirer");
 
 var ALPHABET = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+underlying = ALPHABET[3];
 // dependency for inquirer npm package
 var inquirer = require("inquirer");
 
-// constructor function used to create programmers objects
-function Letter(userGuess, lettersGuess, correctLetter, wrongLetters, dashes, selectedWord) {
+// constructor function used to create Letter objects
+function Letter(userGuess, underlying, checked, correctLetter, wrongLetters, dashes) {
   
-  	this.userGuess = userGuess
-   	this.lettersGuess = lettersGuess;
-	this.correctLetter = correctLetter;
-	this.wrongLetters = wrongLetters;
-	this.dashes = [];
-	this.selectedWord=selectedWord;
+  this.userGuess = userGuess;
+  this.underlying = underlying;//underlying letter
+	this.checked= checked;//boolean value to check if the letter was guesed
+  this.correctLetter = correctLetter;//correct guesses (no repeat letters)
+	this.wrongLetters = wrongLetters;//incorrect guesses
+	this.dashes ='_';                 // display of the word
+	this.checkIfGuessed = function() {
+    if (this.checked === true) {
+      this.correctLetter = this.userGuess;
+      console.log(this.correctLetter);
+    }
+    else {
+      this.wrongLetters= this.userGuess;
+      console.log(this.dashes); 
+    }
+  };
 }
 
-// creates the printInfo method and applies it to all programmer objects
+// creates the printInfo method and applies it to all letter objects
 Letter.prototype.printInfo = function() {
   console.log("Letter: " + this.userGuess);
 };
 
-Letter.prototype.setWord = function() {
-	var word = "jenniferl";
-	this.selectedWord = word.split('');
-	var self = this;
-	this.selectedWord.forEach(function(letter) {
-		self.dashes.push('-');
-	});
-  /*
-	this.selectedWord.forEach(function (letter, i) {
-		if (thisuserGuess === letter) {
-			self.dashes[i] = letter;
-		}
-	});
-  */
-}
-/*
+// runs inquirer and asks the user a series of questions whose replies are
+// stored within the variable answers inside of the .then statement
+inquirer.prompt([  
+  {
+    name: "userGuess",
+    message: "Name a letter"
+  }
 
-var constructWord = function(completeObject){
- for (var i=0; i<word.length; i++ )
-    {completeObject.WORD= completeObject.WORD.replace("[-]"),completeObject.letter[i]  }
+]).then(function(answers) {
+  // initializes the variable newLetter to be a letter object which will take
+  // in all of the user's answers to the questions above
+  var newLetter = new Letter(  
+    answers.userGuess
+  );
+  //console.log("Checking: " + newLetter.userGuess);
+  newLetter.printInfo();
 
-console.log(completeObject.WORD);
-}
-	
-*/
+  //check if underlying===userGuess
 
-// variable we will use to count how many times our questions have been asked
-var count = 0;
-
-var askQuestion = function() {
-  // if statement to ensure that our questions are only asked five times
-  if (count < 5) {
-    // runs inquirer and asks the user a series of questions whose replies are
-    // stored within the variable answers inside of the .then statement
-    inquirer.prompt([
-      
-      {
-        name: "userGuess",
-        message: "Name a letter"
-      }
-
-    ]).then(function(answers) {
-      // initializes the variable newguy to be a programmer object which will take
-      // in all of the user's answers to the questions above
-      var newLetter = new Letter(
-        
-        answers.userGuess
-        
-
-        );
-      // printInfo method is run to show that the newguy object was successfully created and filled
-      
-      
-      newLetter.printInfo();
-      newLetter.setWord();
-      //newLetter.makeGuess();
-      console.log(newLetter.dashes);
-
-      //newLetter.makeGuess();
-      // add one to count to increment our recursive loop by one
-      count++;
-      console.log(newLetter.selectedWord);
-      console.log(newLetter.dashes);
-
-	
-      // run the askquestion function again so as to either end the loop or ask the questions again
-      askQuestion();
-    });
-    // else statement which prints "all questions asked" to the console
-    // when the code has been run five times
+  var isCorrect = function(newLetter) {
+  if (newLetter.userGuess===this.underlying) {
+    console.log("CORRECT!");
+    newLetter.checked = true;
   }
   else {
-    console.log("All questions asked");
+      console.log("WRONG");
+      newLetter.checked = false; 
   }
+
 };
-
-// call askquestion to run our code
-askQuestion();
-
-
-
-
-//console.log(newLetter.selectedWord);
-//console.log(newLetter.words);
-//console.log(newLetter.dashes);
-//newLetter.makeGuess('l');
-//console.log(newLetter.dashes);
-//console.log(newLetter);
-
-
-
-/**
- * Hangman class.
- */
-//function Letter(name, userGuess, lettersGuess) {
-	//this.name=name;
-	//this.userGuess = userGuess;
-	//this.lettersGuess = lettersGuess;
-	//this.guessWord = [];
-	//this.correctLetter = [];
-	//this.wrongLetters = [];
-	//this.dashes = [];
-	//this.RemGuess = 15;
-	//this.counter = 0;
-	//this.words = words;
-	//this.selectedWord = [];
-
-	//Programmer.prototype.printInfo = function() {
-  //console.log("Letter: " + this.userGuess);
-//};
-//}
-
-/**
- * Sets selectedWord to a random word.
- */
+  isCorrect(newLetter);
+  newLetter.checkIfGuessed();  
+});
 
 //module.exports = Letter;
